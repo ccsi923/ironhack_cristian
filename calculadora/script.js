782215ce6f739dfa6701475e7f06683f3d90ce78
Cristian  Saavedra Imbajoa <cristian@beauty.local>
2020-05-17 20:29:58 +0200

    HTML, CSS and JS of Calculator

---
+++ calculadora/script.js
@@ -0,0 +1,32 @@
+
+let bttn = document.querySelectorAll(".bttn li");
+let	input = document.querySelector(".input");
+
+function regis() {
+  let bttnVal = this.innerHTML;
+  let inputVal = input.innerHTML;
+  switch (bttnVal) {
+		case "c":
+			input.innerHTML = "";//cleaning
+			break;
+		case "=":
+			let equa = inputVal;
+		  if(equa){
+				try {
+					input.innerHTML = eval(equa); //calculation since string
+				} catch (e) {
+						alert('Incorrect expression');
+						input.innerHTML = "";
+				}
+				}
+			break;
+		default:
+			input.innerHTML += bttnVal;
+			break;
+		}
+}
+
+for (let i = 0; i < bttn.length; i++) {
+		//registering and printing input
+		bttn[i].addEventListener('click', regis);
+}
